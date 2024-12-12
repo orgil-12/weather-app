ddcbb7e81bd8d7417155fa5d51fc2294768335f6
orgil-12 <theorgilbold@gmail.com>
2024-12-12 17:22:26 +0800

    init second code

--- src/app/card.jsx
+++ src/app/card.jsx
@@ -1,24 +1,25 @@
 "use client";
+import { Manrope } from "next/font/google";
+const manrope = Manrope({
+  subsets:["latin"]
+})
 
-import { SearchIcon } from "../componant/SearchIcon";
-
-export default function Card() {
+export default function Card({ value , temperature}) {
+  const gradient = value ==="day" ? "bg-gradient-to-b from-[#111827] to-[#6B7280] " : "bg-gradient-to-b from-[#F9FAFB] to-[#000000]";
+  const nightColor =
+    "bg-gradient-to-b from-[#1F2937] to-[#111827] text-white ";
+  const color = value === "night" ? nightColor : "bg-white";
+  const cardBackground = value ==="night" ? "bg-[#111827]" : "bg-white"
   return (
-    <div className="w-full h-screen flex  ">
-      <div className="w-1/2 h-screen bg-[#F3F4F6] box-border  flex flex-col ">
-        <div className="mt-10 ml-10 w-[567px] h-[80px] rounded-[48px] px-6 py-4 flex  bg-white items-center ">
-          <SearchIcon />
-          <input
-            type="text"
-            placeholder="Search"
-            className="  text-[32px] focus:outline-0 ml-4 "
-          />
+    <div className={`w-[814px] h-[1278px] rounded-[78px] ${cardBackground} flex justify-center ${manrope.className} `}>
+      <div className={`w-[794px] h-[900px] bg-white rounded-[70px] mt-[10px] ${color} flex flex-col`}>
+        <div className="mt-[56px] ml-[100px] ">
+          <p className="text-[30px] text-[#9CA3AF]  ">September 10, 2021</p>
+          <h1 className="text-[88px] font-extrabold ">Krakow</h1>
         </div>
-        <div className="w-[814px] h-[1228px] rounded-[78px] bg-white box-border m-auto ">
-            
-        </div>
+        <img src="/nightCloud.png" className="w-[477px] h-[477px] mx-auto drop-shadow-[0_5px_25px_rgba(255,255,255,0.5)] mt-5 "/>
+        <h1 className={`text-[200px] ml-[100px] font-extrabold text-transparent bg-clip-text ${gradient}`} >{temperature}</h1>
       </div>
-      <div className="w-1/2 h-screen bg-[#0F141E] "></div>
     </div>
   );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,36 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Card from "./card";
-
+import { SearchIcon } from "../componant/SearchIcon";
+import { Manrope } from "next/font/google";
+const manrope = Manrope({
+  subsets: ["latin"],
+});
 export default function Home() {
   return (
-    <div><Card/></div>
+    <div className={`w-full h-screen flex `}>
+      <div
+        className={`w-1/2 h-screen bg-[#F3F4F6]  flex flex-col-reverse items-center justify-between pb-[50px] `}
+      >
+        <Card value="day" temperature="26°" status="bright"/>
+        <SearchInput />
+      </div>
+      <div className="w-1/2 h-screen bg-[url('/bg.png')] bg-no-repeat bg-cover bg-center flex flex-col-reverse items-center  justify-between pb-[50px] ">
+        <Card value="night" temperature="17°" status="clear"/>
+      </div>
+    </div>
   );
 }
+
+const SearchInput = () => {
+  return (
+    <div className="mt-10 ml-10 w-[567px] h-[80px] rounded-[48px] px-6 py-4 flex bg-white items-center ">
+      <SearchIcon />
+      <input
+        type="text"
+        placeholder="Search"
+        className="  text-[32px] focus:outline-0 ml-4 "
+      />
+    </div>
+  );
+};
